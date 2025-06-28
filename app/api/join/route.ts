d8e51c887687a4823b535eebeecd7a3dcf01bf59
Mauricio Melo <mauricioltw@gmail.com>
2025-06-28 15:21:58 -0300

    update script to instal k3s

--- app/api/join/route.ts
+++ app/api/join/route.ts
@@ -1,19 +1,27 @@
 import { exec } from "child_process";
 import util from "util";
-import { z } from "zod";
+import { z } from "zod/v4";
+import path from "path";
 
 const joinSchema = z.object({
   token: z.string().min(1).max(300),
+  serverUrl: z.url().min(1).max(300),
 });
 
 export async function POST(req: Request) {
   const body = await req.json();
-  const { token } = joinSchema.parse(body);
+  const { token, serverUrl } = joinSchema.parse(body);
 
   const execAsync = util.promisify(exec);
 
+  // Get the absolute path to join_cluster.sh to avoid privilege escalation issues
+  const scriptPath = path.resolve(process.cwd(), "join_cluster.sh");
+
+  console.log({ scriptPath });
   // TODO: make sure the token is sanitized to avoid injection attacks
-  const res = await execAsync(`./join_cluster.sh '${token}'`);
+  const res = await execAsync(
+    `K3S_URL=${serverUrl} K3S_TOKEN=${token} ${scriptPath}`
+  );
 
   return Response.json({
     stdout: res.stdout,
