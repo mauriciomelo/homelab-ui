18e59031786bae321a7c078897934cbab6175361
Mauricio Melo <mauricioltw@gmail.com>
2025-06-30 10:17:51 -0300

    fix config schema

--- app/(dashboard)/apps/config.tsx
+++ app/(dashboard)/apps/config.tsx
@@ -4,15 +4,14 @@ const optionalConfig = z.object({
   PUBLISH_MDNS_SERVICE: z.stringbool().default(true),
 });
 
-const clientConfigSchema = z
-  .object({
-    PROJECT_DIR: z.string().min(1, "PROJECT_DIR must be set"),
-    CLUSTER_NAME: z.string().min(1, "CLUSTER_NAME must be set"),
-    USER_NAME: z.string().min(1, "USER_NAME must be set"),
-    USER_EMAIL: z.email("USER_EMAIL must be a valid email"),
-    GITHUB_TOKEN: z.string().min(1, "GITHUB_TOKEN must be set"),
-  })
-  .extend(optionalConfig);
+const clientConfigSchema = z.object({
+  ...optionalConfig.shape,
+  PROJECT_DIR: z.string().min(1, "PROJECT_DIR must be set"),
+  CLUSTER_NAME: z.string().min(1, "CLUSTER_NAME must be set"),
+  USER_NAME: z.string().min(1, "USER_NAME must be set"),
+  USER_EMAIL: z.email("USER_EMAIL must be a valid email"),
+  GITHUB_TOKEN: z.string().min(1, "GITHUB_TOKEN must be set"),
+});
 
 export function getAppConfig() {
   return clientConfigSchema.parse(process.env);
